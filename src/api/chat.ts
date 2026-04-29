2807761f6539405eab4248e320ed00c10c540062
fernando <413114463@qq.com>
2026-04-29 15:23:49 +0800

    修复占位函数

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,11 +1,11 @@
 import {bunApi} from '../utils/requestGenerator'
 
 export const uploadImageToOss = () => {
-    return bunApi("/chat/a")
+    return bunApi.post("/chat/a")
 } 
 export const streamChat  = () => {
-    return bunApi("/chat/a")
+    return bunApi.post("/chat/a")
 } 
 export const clearChatHistory = (threadId: string) => {
-    return bunApi("/chat/a")
+    return bunApi.post("/chat/a")
 } 
