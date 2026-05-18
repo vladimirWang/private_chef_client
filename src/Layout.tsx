c6a43022dcec0e3db7fcdc55335460d8e260d483
fernando <413114463@qq.com>
2026-05-18 12:33:56 +0800

    layout页加底部状态栏

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,50 @@
 import { Outlet } from "react-router-dom";
+import {
+    AppOutline,
+    MessageOutline,
+    MessageFill,
+    UnorderedListOutline,
+    UserOutline,
+  } from 'antd-mobile-icons'
+import { TabBar } from "antd-mobile";
 
 export default function Layout() {
+    const tabs = [
+        {
+          key: 'home',
+          title: '首页',
+          icon: <AppOutline />,
+        //   badge: Badge.dot,
+        },
+        {
+          key: 'todo',
+          title: '待办',
+          icon: <UnorderedListOutline />,
+          badge: '5',
+        },
+        {
+          key: 'message',
+          title: '对话',
+          icon: (active: boolean) =>
+            active ? <MessageFill /> : <MessageOutline />,
+          badge: '99+',
+        },
+        // {
+        //   key: 'personalCenter',
+        //   title: '我的',
+        //   icon: <UserOutline />,
+        // },
+      ]
     return (
         <div>
-            <Outlet />
+            <section style={{paddingBottom: '50px'}}>
+                <Outlet />
+            </section>
+            <TabBar style={{ position: 'fixed', bottom: 0, left: 0, right: 0, background: 'white' }}>
+                {tabs.map(item => (
+                    <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                ))}
+            </TabBar>
         </div>
     )
 }
